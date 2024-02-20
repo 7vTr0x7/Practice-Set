7f559eaa772e117653bd9c2782a028ea556bf4a3
7vTr0x7 <7vtrox7@gmail.com>
2024-02-20 16:31:31 +0530

    Practice rev-set-1 q1

---
+++ rev-set-1/q1.js
@@ -0,0 +1,70 @@
+const studentsMarks = [
+  {
+    name: "Preethi",
+    rollNo: 301,
+    chemistry: 78,
+    maths: 88,
+    commerce: 90,
+    physicalEducation: 90,
+  },
+  {
+    name: "Priya",
+    rollNo: 302,
+    chemistry: 75,
+    maths: 90,
+    commerce: 85,
+    physicalEducation: 94,
+  },
+  {
+    name: "Jaya",
+    rollNo: 303,
+    chemistry: 82,
+    maths: 95,
+    commerce: 88,
+    physicalEducation: 92,
+  },
+  {
+    name: "Prema",
+    rollNo: 304,
+    chemistry: 85,
+    maths: 96,
+    commerce: 91,
+    physicalEducation: 99,
+  },
+  {
+    name: "Shreetama",
+    rollNo: 305,
+    chemistry: 83,
+    maths: 92,
+    commerce: 89,
+    physicalEducation: 91,
+  },
+];
+
+function calculateAndTotalMarks() {
+  for (let i = 0; i < studentsMarks.length; i++) {
+    let total =
+      studentsMarks[i].chemistry +
+      studentsMarks[i].commerce +
+      studentsMarks[i].maths +
+      studentsMarks[i].physicalEducation;
+
+    studentsMarks[i].totalMarks = total;
+  }
+}
+
+calculateAndTotalMarks();
+
+console.log("----------- ------------- -----------");
+console.log(studentsMarks);
+console.log("----------- ------------- -----------");
+
+function getStudentsWithTotalMarksAbove345() {
+  for (let i = 0; i < studentsMarks.length; i++) {
+    if (studentsMarks[i].totalMarks > 345) {
+      console.log("Student Name:", studentsMarks[i].name);
+    }
+  }
+}
+
+getStudentsWithTotalMarksAbove345();
